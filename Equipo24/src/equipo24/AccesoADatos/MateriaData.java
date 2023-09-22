7048be5d51100c3c471008432a3b1319fd6c2b2c
sastremax <141637332+sastremax@users.noreply.github.com>
2023-09-22 12:17:46 -0300

    se actualiza materia data y materia
    se comentan las dos clases

--- Equipo24/src/equipo24/AccesoADatos/MateriaData.java
+++ Equipo24/src/equipo24/AccesoADatos/MateriaData.java
@@ -9,43 +9,75 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author calga
- */
 public class MateriaData {
 
     private Connection con = null;
 
+// Constructor que establece la conexión a la base de datos al crear una instancia de la clase
     public MateriaData() {
 
         con = Conexion.getConexion();
 
     }
+// Método para guardar una materia en la base de datos
 
     public void guardarMateria(Materia materia) {
         String sql = "INSERT INTO materia (nombre, año, estado) VALUES (?, ?, ?)";
-
+        /*
+  Esta línea crea un objeto PreparedStatement llamado ps utilizando una conexión
+a la base de datos (con) y una consulta SQL preparada (sql). Un PreparedStatement
+es una forma de ejecutar consultas SQL en Java que pueden contener parámetro
+         */
         try (PreparedStatement ps = con.prepareStatement(sql)) {
+            /*
+La primera línea establece el primer parámetro de la consulta SQL preparada (?)
+con el valor de materia.getNombre(). En otras palabras, asigna el valor del 
+nombre de la materia,obtenido a través del método getNombre() del objeto materia,
+al primer parámetro de la consulta SQL.
+             */
             ps.setString(1, materia.getNombre());
             ps.setInt(2, materia.getAniomateria());
             ps.setBoolean(3, materia.isEstado());
+            /*  esta línea ejecuta la consulta SQL preparada. 
+es una consulta de inserción en la que se están insertando datos (el nombre, el año 
+y el estado de una materia) en la base de datos. El método executeUpdate() se
+utiliza para ejecutar consultas que modifican los datos en la base de datos, 
+como inserciones, actualizaciones o eliminaciones.
+             */
             ps.executeUpdate();
         } catch (SQLException e) {
+// Captura y muestra cualquier excepción SQL que ocurra
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia: " + e.getMessage());
         }
     }
+// Método para buscar una materia por su ID en la base de datos
 
     public Materia buscarMateria(int id) {
+        /*
+Se declara una variable materia de tipo Materia e inicialmente se establece en null.
+Esta variable se utilizará para almacenar la materia encontrada en la base de datos.
+         */
+
         Materia materia = null;
+        /*
+Se define una cadena sql que contiene una consulta SQL. La consulta selecciona 
+todos los campos de la tabla "materia" donde el ID de la materia coincida con el
+valor del parámetro id (representado por ?, que será reemplazado más adelante)
+         */
         String sql = "SELECT * FROM materia WHERE idMateria = ?";
-
+// Se inicia un bloque try-catch para manejar posibles excepciones SQL
         try {
+// Se crea un objeto PreparedStatement utilizando la conexión `con` y la consulta `sql`
             PreparedStatement ps = con.prepareStatement(sql);
+// Se establece el valor del parámetro en la consulta SQL con el valor de `id`
             ps.setInt(1, id);
+// Se ejecuta la consulta SQL y se obtiene un conjunto de resultados ResultSet
             ResultSet rs = ps.executeQuery();
-
+// Si hay al menos un resultado en el conjunto de resultados
             if (rs.next()) {
+                /* Si se encuentra una materia con el ID proporcionado, se crea un objeto Materia 
+y se llena con los datos de la base de datos
+                 */
                 materia = new Materia();
                 materia.setIdMateria(rs.getInt("idMateria"));
                 materia.setNombre(rs.getString("nombre"));
@@ -57,33 +89,34 @@ public class MateriaData {
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia " + ex.getMessage());
         }
-
+// Devuelve la lista de materias
         return materia;
     }
 
+// Método para modificar una materia en la base de datos
     public void modificarMateria(Materia materia) {
 
         String sql = "UPDATE materia SET nombre=?,año=?,estado=? WHERE idMateria = ?";
-
+// Se declara un objeto PreparedStatement en null
         PreparedStatement ps = null;
 
         try {
-
+// Se crea un PreparedStatement a partir de la consulta SQL
             ps = con.prepareStatement(sql);
 
-            ps.setString(1, materia.getNombre());
+            ps.setString(1, materia.getNombre()); // Se establece el nombre de la materia
             ps.setInt(2, materia.getAniomateria());
             ps.setBoolean(3, materia.isEstado());
             ps.setInt(4, materia.getIdMateria());
-
+// Se ejecuta la consulta SQL de actualización y obtiene el número de filas afectadas
             int exito = ps.executeUpdate();
-
+// Si la consulta se ejecuta exitosamente (una fila modificada), muestra un mensaje de éxito
             if (exito == 1) {
 
                 JOptionPane.showMessageDialog(null, "Se ha modificado exitosamente.");
 
             }
-
+// Captura y muestra un mensaje de error en caso de excepción SQL
         } catch (SQLException ex) {
 
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia " + ex.getMessage());
@@ -91,20 +124,23 @@ public class MateriaData {
 
     }
 
+    /* Método para eliminar una materia en la base de datos. en realidad no lo elimina 
+    solo cambia su estado a 0
+     */
     public void eliminarMateria(int id) {
 
         try {
-
+// Se define la consulta SQL para cambiar el estado de la materia a 0
             String sql = "UPDATE materia SET estado = 0 WHERE idMateria = ? ";
-
+// Se crea un objeto PreparedStatement
             PreparedStatement ps = con.prepareStatement(sql);
-
+// Se establece el valor del parámetro en la consulta SQL con el valor de `id`
             ps.setInt(1, id);
-
+// Se ejecuta la consulta SQL de actualización y obtiene el número de filas afectadas
             int fila = ps.executeUpdate();
 
             if (fila == 1) {
-
+// Si se actualiza una fila, se muestra un mensaje indicando que se eliminó la materia
                 JOptionPane.showMessageDialog(null, " Se elimino la materia.");
 
             }
@@ -118,20 +154,25 @@ public class MateriaData {
         }
     }
 
+    /*
+Método Este código en Java se encarga de recuperar una lista de todas las materias
+almacenadas en la base de datos y devolverlas como una lista de objetos Materia.
+     */
     public List<Materia> listarMaterias() {
-
+        // Se crea una lista vacía que contendrá las materias
         List<Materia> materia = new ArrayList<>();
 
         try {
-
+// Se define la consulta SQL para seleccionar todas las materias de la base de datos
             String sql = "SELECT * FROM materia";
-
+// Se crea un objeto PreparedStatement a partir de la consulta SQL
             PreparedStatement ps = con.prepareStatement(sql);
-
+// Se ejecuta la consulta SQL y obtiene un conjunto de resultados ResultSet
             ResultSet rs = ps.executeQuery();
-
+// Se inicia un bucle "while" para recorrer los registros en el ResultSet
             while (rs.next()) {
 
+// Por cada registro de materia en la base de datos, se crea un objeto Materia y se agrega a la lista            
                 Materia mat = new Materia();
 
                 mat.setIdMateria(rs.getInt("idMateria"));
@@ -141,11 +182,11 @@ public class MateriaData {
                 mat.setAniomateria(rs.getInt("año"));
 
                 mat.setEstado(rs.getBoolean("estado"));
-
+// Se agrega el objeto "mat" a la lista "materia"
                 materia.add(mat);
 
             }
-
+// Se cierra el objeto PreparedStatement "ps" para liberar recursos
             ps.close();
 
         } catch (SQLException ex) {
@@ -153,10 +194,9 @@ public class MateriaData {
             JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Materia" + ex.getMessage());
 
         }
-
+// Se devuelve la lista "materia" que contiene todas las materias recuperadas
         return materia;
 
     }
 
-  
 }
--- Equipo24/src/equipo24/Entidades/Materia.java
+++ Equipo24/src/equipo24/Entidades/Materia.java
@@ -1,25 +1,41 @@
 package equipo24.Entidades;
 
-/**
- *
- * @author calga
- */
 public class Materia {
-    
-        private int idMateria;
-        private String nombre;
-        private int aniomateria;
-        private boolean estado;
 
+    /*
+    Se definen los atributos (variables miembro) de la clase Materia.
+    La clase tiene cuatro atributos privados: idMateria (un entero), 
+    nombre (una cadena de texto), aniomateria (un entero) y estado (un valor booleano).
+    */
+    
+    private int idMateria;
+    private String nombre;
+    private int aniomateria;
+    private boolean estado;
+
+    // constructor predeterminado de la clase Materia. Vacio. No toma argumentos.
+    
     public Materia() {
     }
 
+    /*
+    un constructor de la clase Materia que toma tres argumentos: nombre,
+    aniomateria y estado. Estos argumentos se utilizan para inicializar 
+    los atributos de la clase.
+    */
+    
     public Materia(String nombre, int aniomateria, boolean estado) {
         this.nombre = nombre;
         this.aniomateria = aniomateria;
         this.estado = estado;
     }
 
+    /*
+    otro constructor de la clase Materia que toma cuatro argumentos: idmateria, 
+    nombre, aniomateria y estado. Estos argumentos se utilizan para inicializar 
+    los atributos de la clase.    
+    */
+    
     public Materia(int idMateria, String nombre, int aniomateria, boolean estado) {
         this.idMateria = idMateria;
         this.nombre = nombre;
@@ -27,6 +43,12 @@ public class Materia {
         this.estado = estado;
     }
 
+    /*
+    Estos son métodos getter (getIdMateria) y setter (setIdMateria)
+    para el atributo idMateria. Los métodos getter se utilizan para obtener 
+    el valor del atributo, y los setter se utilizan para establecer el valor del atributo
+    */
+    
     public int getIdMateria() {
         return idMateria;
     }
@@ -58,45 +80,16 @@ public class Materia {
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
- 
+
+    /*
+    El método toString esta sobrescrito. Devuelve una representación de cadena
+    de la materia en forma de "nombre aniomateria°". Este método se utiliza para 
+    convertir una instancia de la clase Materia
+    */
+    
     @Override
     public String toString() {
-        return nombre+ " " +aniomateria + "°";
+        return nombre + " " + aniomateria + "°";
     }
-       
-    
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
